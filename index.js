fd9c5127fd1d0d1f8bc409151d315c0833e4bc7d
Hamid Vahed <hamid.vahed@gmx.de>
2023-02-23 12:23:01 +0100

    create quiz-app

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // phew… not a lot going on here. Please add some code!
+const toggleBtn = document.querySelector('[data-js="bookmark-btn"]');
+toggleBtn.addEventListener("click", handleBookMarkClick);
+
+function handleBookMarkClick() {
+  toggleBtn.classList.toggle("bookmark--active");
+}
